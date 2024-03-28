17687785aaefaa09c134b9193da8142d7d895ca7
kumulynja <b@kumuly.dev>
2024-03-28 17:13:47 +0100

    working app

--- lib/services/wallets/impl/bitcoin_wallet_service.dart
+++ lib/services/wallets/impl/bitcoin_wallet_service.dart
@@ -34,9 +34,7 @@ class BitcoinWalletService implements WalletService {
   Future<void> addWallet() async {
     // 1. Replace the hardcoded test mnemonic with the code to create a new
     //   mnemonic with 12 words every time this function is called.
-    final mnemonic = await Mnemonic.fromString(
-      'test test test test test test test test test test test test',
-    );
+    final mnemonic = await Mnemonic.create(WordCount.words12);
 
     await _mnemonicRepository.setMnemonic(
       await mnemonic.asString(),
@@ -45,7 +43,7 @@ class BitcoinWalletService implements WalletService {
     await _initWallet(mnemonic);
 
     print(
-      'Wallet added with mnemonic: ${mnemonic.asString()} and initialized!',
+      'Wallet added with mnemonic: ${await mnemonic.asString()} and initialized!',
     );
   }
 
@@ -60,6 +58,9 @@ class BitcoinWalletService implements WalletService {
     if (!hasWallet) return;
 
     // 7. Sync the wallet with the blockchain
+    await _wallet!.sync(
+      blockchain: _blockchain,
+    );
   }
 
   @override
@@ -68,21 +69,41 @@ class BitcoinWalletService implements WalletService {
 
     // 8. Get the balance of the wallet and return the spendable part of it.
     //  For testing purposes, you can just print out the other parts of the balance as well.
-    return 0;
+    final balance = await _wallet!.getBalance();
+
+    print('Confirmed balance: ${balance.confirmed}');
+    print('Spendable balance: ${balance.spendable}');
+    print('Unconfirmed balance: ${balance.immature}');
+    print('Trusted pending balance: ${balance.trustedPending}');
+    print('Pending balance: ${balance.untrustedPending}');
+    print('Total balance: ${balance.total}');
+
+    return balance.spendable;
   }
 
   @override
   Future<String> generateInvoice() async {
     // 9. Get a new unused address from the wallet and return it as a String.
+    final invoice = await _wallet!.getAddress(
+      addressIndex: const AddressIndex.increase(),
+    );
 
-    return '';
+    return invoice.address;
   }
 
   @override
   Future<List<TransactionEntity>> getTransactions() async {
     // 10. Get the list of transactions from the wallet and return them as a list of `TransactionEntity` instances.
+    final transactions = await _wallet!.listTransactions(includeRaw: false);
 
-    return [];
+    return transactions.map((tx) {
+      return TransactionEntity(
+        id: tx.txid,
+        receivedAmountSat: tx.received,
+        sentAmountSat: tx.sent,
+        timestamp: tx.confirmationTime?.timestamp,
+      );
+    }).toList();
   }
 
   @override
@@ -93,26 +114,43 @@ class BitcoinWalletService implements WalletService {
     int? absoluteFeeSat,
   }) async {
     // 11. Convert the invoice String to a BDK Address type
+    final address = await Address.fromString(
+      s: invoice,
+      network: Network.signet,
+    );
 
     // 12. Use the address to get the script that would lock a transaction output to the address
+    final script = await address
+        .scriptPubkey(); // Creates the output scripts so that the wallet that generated the address can spend the funds
 
     // 13. Initialize a `TxBuilder` instance.
+    final txBuilder = TxBuilder();
 
     // 14. Add the recipient and the amount to send to the transaction builder.
+    txBuilder.addRecipient(script, amountSat);
 
     // 15. Set the fee rate for the transaction based on the provided fee rate or absolute fee on the transaction builder.
+    if (satPerVbyte != null) {
+      txBuilder.feeRate(satPerVbyte);
+    } else if (absoluteFeeSat != null) {
+      txBuilder.feeAbsolute(absoluteFeeSat);
+    }
 
     // 16. Enable RBF (Replace-By-Fee) on the transaction builder
+    txBuilder.enableRbf();
 
     // 17. Finish the transaction building
+    final (psbt, _) = await txBuilder.finish(_wallet!);
 
     // 18. Sign the transaction with the wallet
+    await _wallet!.sign(psbt: psbt);
 
     // 19. Extract the transaction as bytes from the finalized and signed PSBT
+    final tx = await psbt.extractTx();
 
     // 20. Broadcast the transaction to the network with the `Blockchain` instance,
     //  this should return the transaction id.
-    final txId = "";
+    final txId = await _blockchain.broadcast(transaction: tx);
 
     print('Transaction broadcasted: $txId');
 
@@ -140,13 +178,32 @@ class BitcoinWalletService implements WalletService {
 
   Future<void> _initWallet(Mnemonic mnemonic) async {
     // 2. Create the master secret key from the mnemonic
+    final secretKey = await DescriptorSecretKey.create(
+      network: Network.signet,
+      mnemonic: mnemonic,
+    );
 
     // 3. Get a BIP84 template descriptor to derive Native SegWit addresses from the secret key to receive external funds (external keychain)
-
+    final receivingDescriptor = await Descriptor.newBip84(
+      secretKey: secretKey,
+      network: Network.signet,
+      keychain: KeychainKind.externalChain,
+    );
     // 4. Get a BIP84 template descriptor to derive Native SegWit addresses from the secret key to receive change (internal keychain)
+    final changeDescriptor = await Descriptor.newBip84(
+      secretKey: secretKey,
+      network: Network.signet,
+      keychain: KeychainKind.internalChain,
+    );
 
     // 5. Create a `Wallet` instance with the descriptors to initialize the `_wallet` field
     //  Use an in-memory database for testing purposes.
+    _wallet = await Wallet.create(
+      descriptor: receivingDescriptor,
+      changeDescriptor: changeDescriptor,
+      network: Network.signet,
+      databaseConfig: const DatabaseConfig.memory(),
+    );
   }
 
   Future<void> _initBlockchain() async {
@@ -154,5 +211,13 @@ class BitcoinWalletService implements WalletService {
     //  `Blockchain` class and configuring it to use an Esplora server on Signet.
     //  For testing purposes, you can use the following Esplora server url:
     //  https://mutinynet.com/api
+    _blockchain = await Blockchain.create(
+      config: const BlockchainConfig.esplora(
+        config: EsploraConfig(
+          baseUrl: 'https://mutinynet.com/api/',
+          stopGap: 10,
+        ),
+      ),
+    );
   }
 }
